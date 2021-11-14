f1b9637df633ed25e2e267a88efe3ba8d8c50ce8
Alfredo Jahn <jahn4563@gmail.com>
2021-11-14 14:53:21 -0400

    jwt generated

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const { body, validationResult } = require('express-validator');
 const User = require('../../models/User');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 // @route   POST api/users
 // @desc    Test route
@@ -53,7 +55,23 @@ router.post(
       await user.save();
 
       // Return JWT
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 36000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
